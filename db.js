8b1eda7786e978c2719314a74e5d475a94978d4e
Paula Reis <129560962+mpaulareis@users.noreply.github.com>
2024-11-07 01:53:30 -0300

    tirei minha senha ne kk

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',  
   user: 'root',  
-  password: 'mp230904',  
+  password: '.',  
   database: 'vote_verde'  
 });
 
